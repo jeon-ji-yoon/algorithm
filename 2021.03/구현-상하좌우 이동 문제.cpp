5535748aa213e9feeee52e63c4524e58248a0ee1
jeon-ji-yoon <70752587+jeon-ji-yoon@users.noreply.github.com>
2021-03-15 00:06:21 +0900

    initial commit

---
+++ 2021.03/구현-상하좌우 이동 문제.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stdio.h> 
+
+using namespace std;
+/*
+계획서 대로 이동했을때 도착하게 되는 좌표를 구하는 문제*/
+
+int n;
+string plans;
+int x = 1;
+int y = 1; // x좌표와 y좌표를 나타낸다
+
+int main(void){
+	cin >> n; //정사각형의 크기를 입력한다 
+	cin.ignore();
+	getline(cin,plans); //공백을 포함하여 알파벳들을 입력받기 
+	for(plans의 길이만큼 for문을 돈다){
+		
+	}
+	
+	
+	/*
+	for(v를 돌면서){
+		if(v[i] == "R"){
+			if(x좌표 > 범위 && y좌표 > 범위 ){
+				//좌표를 이동시킨다
+			}
+			
+		}
+		else if(v[i] == "L"){
+			if(x좌표 > 범위 && y좌표 > 범위 ){
+				//좌표를 이동시킨다
+			}
+			
+		}
+		else if(v[i] == "U"){
+			if(x좌표 > 범위 && y좌표 > 범위 ){
+				//좌표를 이동시킨다
+			}
+			
+		}
+		else if(v[i] == "D"){
+			 
+			if(x좌표 > 범위 && y좌표 > 범위 ){
+				//좌표를 이동시킨다
+			}
+		}
+	
+		
+	} */
+	
+	return 0;
+}
